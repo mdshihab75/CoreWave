56c2ad831641c09912e58d52cd36f5de8826f8cb
mdshihab75 <mostakimshihab75@gmail.com>
2026-05-02 22:11:49 +0600

    Project Part Start Now

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
                 <div className='w-2/4'>
                 <h2 className='font-Lato font-semibold text-[45px] leading-13.5 text-primary-black pr-25 mb-7.25'>Experience the power of Corewave</h2>
                 <p className='font-Lato font-normal text-base leading-6 text-primary-gray pr-15'>Are you ready to take your business to the next level? Look no further than Corewave. Our innovative technology and expert team can help you unlock your business's full potential. By harnessing the power of Corewave, you can streamline your operations, improve efficiency</p>
-                <button className='font-Lato font-semibold text-base text-primary-green px-6.25 py-[14.5px] bg-banner-mix rounded-[5px] mt-[59.5px] cursor-pointer'>Learn More</button>
+                <button className='font-Lato font-semibold text-base text-primary-green px-6.25 py-[14.5px] bg-seventh-color rounded-[5px] mt-[59.5px] cursor-pointer'>Learn More</button>
             </div>
             <div className='w-2/4'>
                 <img className='rounded-bl-[50px] rounded-br-[50px] rounded-tl-[50px]' src={GroupImage} alt="group" />
